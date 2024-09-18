48842fc847958f77dad7b606c3cb57f2a8f54303
kenziehh <ghaisani.nurani@gmail.com>
2024-09-18 13:27:09 +0700

    `feat(auth)` : finish credential login

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/sections/login/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <main className="flex h-screen justify-center items-center">
+      <LoginForm />
+    </main>
+  );
+}
