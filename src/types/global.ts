d8ece5e760be9112eb0a79d9e4bc29b00339bdd1
kenziehh <ghaisani.nurani@gmail.com>
2024-09-19 23:33:46 +0700

    `feat(auth)` : decode jwt to get user id

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,8 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface User {
+  UserId: string;
   username: string;
   name: string;
   email: string;
   password: string;
 }
 
+interface DecodedJWT {
+  UserId: string;
+  exp: number;
+}
