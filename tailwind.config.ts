cdd1b8240b19f46c5cd641f7c51b78cd818d22c9
kenziehh <ghaisani.nurani@gmail.com>
2024-09-26 22:23:36 +0700

    `style` : add success, warning, and danger color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,27 @@ const config: Config = {
         "secondary-pale-cream": "#DADAC9",
         "secondary-pale-yellow": "#F4FFC4",
         "secondary-pale-blue": "#DADFDF",
+        success: {
+          "100": "#20793A",
+          "80": "#2AA24E",
+          "60": "#62D083",
+          "40": "#89DCA2",
+          "20": "#EBFAF0",
+        },
+        danger: {
+          "100": "#990000",
+          "80": "#CC0100",
+          "60": "#FF666A",
+          "40": "#FF9999",
+          "20": "#FFE5E5",
+        },
+        warning: {
+          "100": "#CB9701",
+          "80": "#FFBD00",
+          "60": "#FFD766",
+          "40": "#FFE599",
+          "20": "#FFF8E5",
+        },
       },
       container: {
         center: true,
