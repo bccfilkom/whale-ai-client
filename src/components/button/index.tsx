f76829be8d7f35c68bdf6b8d5be2bef1cfa17ed4
kenziehh <ghaisani.nurani@gmail.com>
2024-10-02 04:39:11 +0700

    `feat` : slicing register page and add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,31 @@
+import React, { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: "primary" | "white";
+}
+
+export const variantStyles = {
+  primary:
+    "bg-gradient-to-r from-gradient-blue-start to-gradient-blue-end text-white hover:bg-light-blue-text ",
+  white: "bg-white text-black hover:bg-gray",
+};
+
+const Button = ({
+  variant = "primary",
+  className = "",
+  disabled,
+  children,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={`rounded-xl ${variantStyles[variant]} text-white py-2 px-3 ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
