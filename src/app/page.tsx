ffe1c54426cefee81dfee9f37a95d189f4e21875
kenziehh <ghaisani.nurani@gmail.com>
2024-10-02 15:10:21 +0700

    `feat` : slicing landing page tab-dekstop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Navbar from "@/components/navbar";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/options";
 import Hero from "./sections/home/Hero";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  // if (session) {
-  //   redirect("/dashboard");
-  // }
+  if (session) {
+    redirect("/dashboard");
+  }
   return (
     <main>
       <Navbar />
