739a8b0ae67846924a94e9a7fc33f77003585f60
kenziehh <ghaisani.nurani@gmail.com>
2024-10-04 09:54:48 +0700

    `feat` : market top ten section at homepage

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import MarketHighlight from "@/app/sections/dashboard/main/MarketHighlight";
 import MarketOverview from "@/app/sections/dashboard/main/MarketOverview";
+import MarketTopTen from "@/app/sections/dashboard/main/MarketTopTen";
 
 export default function DashboardPage() {
   return (
     <main>
       <MarketOverview />
       <MarketHighlight />
+      <MarketTopTen />
     </main>
   );
 }
